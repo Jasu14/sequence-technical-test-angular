aa4dd4ff2cb22dbf99c4c0629557057c5414db4a
Jasu14 <44318579+Jasu14@users.noreply.github.com>
2025-04-06 13:49:08 +0200

    RxJS implementation on httpService and ng-templates removed

--- frontend/src/app/components/list/list.component.ts
+++ frontend/src/app/components/list/list.component.ts
@@ -13,42 +13,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent implements OnInit, AfterViewInit {
+export class ListComponent implements OnInit {
 
   currentPath: string = "";
-  listElements: any = [];
-  templateToUse!: TemplateRef<any>
-  displayedColumns: string[] = [];
-
-  @ViewChild('cardSongsViewTemplate') cardSongsViewTemplate!: TemplateRef<any>;
-  @ViewChild('cardArtistsViewTemplate') cardArtistsViewTemplate!: TemplateRef<any>;
-  @ViewChild('cardCompaniesViewTemplate') cardCompaniesViewTemplate!: TemplateRef<any>;
+  listElements$: any = this.httpService.items$;
 
   constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute, private routeService: RouteService) { }
 
   ngOnInit(): void {
     this.currentPath = this.routeService.getPathFromRoute(this.activatedRoute);
 
-    this.httpService.getList(this.currentPath).then(
-      data => {
-        this.listElements = data;
-        this.displayedColumns = Object.keys(this.listElements[0]);
-      },
-      error => {
-        console.error("Error on init list: ", error);
-      }
-    )
-  }
-
-  ngAfterViewInit(): void {
-    this.setTemplateToUse(this.currentPath);
-  }
-
-  setTemplateToUse(path: string) {
-    switch(path) {
-      case "songs": this.templateToUse = this.cardSongsViewTemplate; break;
-      case "artists": this.templateToUse = this.cardArtistsViewTemplate; break;
-      case "companies": this.templateToUse = this.cardCompaniesViewTemplate; break;
-    }
+    this.httpService.getList(this.currentPath);
   }
 }
